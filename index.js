8035ac0895c6e20734081e88c73af584a64dc78c
rahathossain3 <rahathossain210@gmail.com>
2022-06-30 18:06:16 +0600

    project dependency done

--- index.js
+++ index.js
@@ -15,12 +15,30 @@ app.use(cors());
 app.use(express.json());
 
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.y5m5n.mongodb.net/?retryWrites=true&w=majority`;
+
+//mongodb client
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
 async function run() {
     try {
         await client.connect();
-        // console.log('database connected')
-        //service 
-        const serviceCollection = client.db('doctors_portal').collection('services');
+        console.log('database connected')
+        //service
+        // const serviceCollection = client.db('doctors_portal').collection('services');
+
+
+
+
+
+
+
+
+
+
 
 
     }
@@ -39,9 +57,9 @@ run().catch(console.dir);
 // console.log(uri);
 
 app.get('/', (req, res) => {
-    res.send('Hello form doctor Uncle!')
+    res.send('Hello form Biller information!')
 })
 
 app.listen(port, () => {
-    console.log(`Doctors app listening on port ${port}`)
+    console.log(`Power Distribution app listening on port ${port}`)
 })
